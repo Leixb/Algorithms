cabdd05e88a82de0bf54a0e92bdaaabcc683808f
Leixb <leix@localhost.localdomain>
2016-02-12 12:21:32 +0100

    Improved and fixed bugs in fraction class Started new geometry class

--- geometry.cpp
+++ geometry.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
-//#include <vector>
 using namespace std;
+
+typedef long T;
+
+class coord {
+public:
+    T x,y,z;
+
+    coord (T X=0, T Y=0, T Z=0) : x(X), y(Y), z(Z) {}
+}
 
 int main () {
 }
